ad76074b3f5e82aeaae65271b787135efb456abb
Apoorv Yadav <apoorvyadav1111@gmail.com>
2021-11-27 17:08:57 +0530

    Removed Secret Key

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3n1eb)5jg&wyi2+&#z3n2c5om6xme5fnjklm7is8dt@7lrej*q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
